27a8302e2fe9cda3a2e924589f7d3eb159f91ff1
liam wheeler <liam_wheeler@hotmail.com>
2019-06-11 09:59:11 +0100

    Initial Commit

---
+++ LinqFeatures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] developers = new Employee[]
+            {
+                new Employee {Id = 1, Name = "Liam"},
+                new Employee {Id = 2, Name = "Warren"}
+            };
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee {Id = 3, Name = "Dec"}
+            };
+        }
+    }
+}
