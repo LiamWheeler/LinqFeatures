9ddcdaa0daf97b6dda8a30c2511451d742017620
liam wheeler <liam_wheeler@hotmail.com>
2019-06-11 10:19:49 +0100

    IEnumerable

--- LinqFeatures/Program.cs
+++ LinqFeatures/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +10,22 @@ namespace LinqFeatures
     {
         static void Main(string[] args)
         {
-            Employee[] developers = new Employee[]
+            IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Liam"},
                 new Employee {Id = 2, Name = "Warren"}
             };
-            List<Employee> sales = new List<Employee>()
+            IEnumerable<Employee> sales = new List<Employee>()
             {
                 new Employee {Id = 3, Name = "Dec"}
             };
+
+            IEnumerator<Employee> enumerator = sales.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
         }
     }
 }
