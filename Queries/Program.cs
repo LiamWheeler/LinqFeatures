95f331ecca05ff13ef85af3cf07a99077ca9e386
liam wheeler <liam_wheeler@hotmail.com>
2019-06-12 10:28:36 +0100

    Query for Movies by Year

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -13,10 +13,17 @@ namespace Queries
             var movies = new List<Movie>
             {
                 new Movie {Title = "The Dark Knight", Rating = 8.9f, Year = 2008},
-                new Movie {Title = "TThe King's Speech", Rating = 8.0f, Year = 2010},
+                new Movie {Title = "The King's Speech", Rating = 8.0f, Year = 2010},
                 new Movie {Title = "Casablanca", Rating = 8.5f, Year = 1942},
                 new Movie {Title = "Star Wars V", Rating = 8.7f, Year = 1980}
             };
+            var query = movies.Where(m => m.Year > 2000);
+
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+
+            }
         }
     }
 }
