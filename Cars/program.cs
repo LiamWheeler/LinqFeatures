c1a0fdbdfd35c226422fa7e869d612d0fafd80d8
liam wheeler <liam_wheeler@hotmail.com>
2019-06-13 11:41:03 +0100

    sorting car by efficiency

--- Cars/program.cs
+++ Cars/program.cs
@@ -12,9 +12,12 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
-            foreach (var car in cars)
+
+            var query = cars.OrderByDescending(c => c.Combined);
+
+            foreach (var car in query)
             {
-                Console.WriteLine(car.Name);
+                Console.WriteLine($"{car.Name} : {car.Combined}");
             }
         }
 
