8590c2442c05f3da9327c771727ed590ff90e11d
liam wheeler <liam_wheeler@hotmail.com>
2019-06-13 09:26:01 +0100

    Avoiding Deferred query pitfalls with ToList()

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -17,9 +17,9 @@ namespace Queries
                 new Movie {Title = "Casablanca", Rating = 8.5f, Year = 1942},
                 new Movie {Title = "Star Wars V", Rating = 8.7f, Year = 1980}
             };
-            var query = movies.Filter(m => m.Year > 2000)
-                               .Take(1);
+            var query = movies.Filter(m => m.Year > 2000).ToList();
 
+            Console.WriteLine(query.Count());
             var enumerator = query.GetEnumerator();
             while (enumerator.MoveNext())
             {
