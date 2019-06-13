bb9efb7c486e5667f98cb447fb6965f3266c7f4a
liam wheeler <liam_wheeler@hotmail.com>
2019-06-13 09:18:33 +0100

    Deferred execution .Take(1)

--- Queries/MyLinq.cs
+++ Queries/MyLinq.cs
@@ -8,6 +8,7 @@ namespace Queries
 {
     public static class MyLinq
     {
+        
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> source,
                                                 Func<T, bool> predicate)
         {
--- Queries/Program.cs
+++ Queries/Program.cs
@@ -17,12 +17,13 @@ namespace Queries
                 new Movie {Title = "Casablanca", Rating = 8.5f, Year = 1942},
                 new Movie {Title = "Star Wars V", Rating = 8.7f, Year = 1980}
             };
-            var query = movies.Filter(m => m.Year > 2000);
+            var query = movies.Filter(m => m.Year > 2000)
+                               .Take(1);
 
-            foreach (var movie in query)
+            var enumerator = query.GetEnumerator();
+            while (enumerator.MoveNext())
             {
-                Console.WriteLine(movie.Title);
-
+                Console.WriteLine(enumerator.Current.Title);
             }
         }
     }
