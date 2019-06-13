e66f4a5ae31791b8f772fb42ff31e99568d57da0
liam wheeler <liam_wheeler@hotmail.com>
2019-06-13 11:07:38 +0100

    Cars Project

---
+++ Cars/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cars")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cars")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6bed6eb8-9b33-409c-a907-5effc9819256")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cars/program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return
+            File.ReadAllLines(path)
+                .Skip(1)  //skips the first line with the column headers
+                .Where(line => line.Length > 1) //ignores any blank lines, the last line in this case
+                .Select(Car.ParseFromCsv)
+                .ToList();
+        }
+    }
+}
